466e58e321687e5609215fc662f3a572df57968d
codeorbitadmin <codeorbit.online@gmail.com>
2025-10-19 02:13:13 +0530

    Update db.js for Render PostgreSQL

--- server/db.js
+++ server/db.js
@@ -1,11 +1,9 @@
 const { Pool } = require('pg');
 
+// Use your Render PostgreSQL credentials
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'neoretail',
-  password: '051121',
-  port: 5432,
+  connectionString: "postgresql://neoretail_db_user:Lr8CCbAhJiZrWNKawOanzxqRXcAXBAP6@dpg-d3pve8bipnbc73a7g300-a.oregon-postgres.render.com/neoretail_db",
+  ssl: { rejectUnauthorized: false } // Required for Render
 });
 
 pool.connect()
